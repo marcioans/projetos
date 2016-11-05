6d11fea194af7c7b7e10ec06fd21572344ac4f2e
Marcio Costa <marcio.ans@gmail.com>
2016-11-04 20:11:37 -0400

    -add botão logoff -add ação no botão de logoff para sair com confirmação de dialog fragment  -corrigido o problema do botão logoff que abria o menu para sair ficando redundante  -add icone ao projeto com icone asset do android studio

--- app/src/main/java/com/fucapi/projeto/saudenasnuvens/MainActivity.java
+++ app/src/main/java/com/fucapi/projeto/saudenasnuvens/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.design.widget.Snackbar;
 //import android.support.v4.app.Fragment;
 
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,8 +20,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+//import android.view.Menu;
+//import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.fucapi.projeto.saudenasnuvens.fragment.ConsultaFragment;
@@ -39,6 +42,7 @@ public class MainActivity extends AppCompatActivity
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
     private static FirebaseDatabase fbDatabase;
+    private Menu menu;
     //private int mStackLevel;
 
 
@@ -135,35 +139,21 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_itens, menu);
-        return true;
-    }
+     public boolean onCreateOptionsMenu(Menu menu) {
+         // Inflate the menu; this adds items to the action bar if it is present.
+         getMenuInflater().inflate(R.menu.menu_main, menu);
+         return super.onCreateOptionsMenu(menu);
+     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-
+        //MyMessageHandler.debug("menu item selected");
+        switch(item.getItemId()){
             case R.id.action_opcao1:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-                //getSupportFragmentManager().beginTransaction()
-                //        .replace(R.id.frame_container, new MyDialogFragment(1, 2)).commit();
-                //openDialogFragment();
-                //mFirebaseAuth.signOut();
-                //startActivity(new Intent(this, EmailPasswordActivity.class));
-
                 showDialog();
-
                 return true;
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
         }
+        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
